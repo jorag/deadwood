faff1f7ac5f0746a077c72f228eeec5668b7d8b3
jorag <jorgen.agersborg@uit.no>
2018-08-07 12:14:14 +0200

    Updated load_data.py to use tkinter file dialog for selecting files.
    When testing I could add a try-catch statement to see if the data is already loaded and/or look for a local file with paths (that can be added to .gitignore).

--- load_data.py
+++ load_data.py
@@ -1,14 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import gdal
-from gdalconst import *
+#import gdal
+#from gdalconst import *
 import os, sys, time
-from src import dispms
+#from src import dispms
 import math
+import tkinter
+from tkinter import filedialog
 
-filename1 = 'test.tif'
-dataset1 = gdal.Open(filename1)
 
-!gdalinfo filename1
+root = tkinter.Tk()
+root.withdraw()
+
+file_path = tkinter.filedialog.askopenfilename()
+
+# Files could be loaded using SNAPPY import product, but for now assuming that the input is .tiff is ok
+
+#filename1 = 'test.tif'
+#dataset1 = gdal.Open(filename1)
+
+#!gdalinfo filename1
 
 # Load data
