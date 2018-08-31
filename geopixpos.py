79ce712bb157c6b6fc8a25a9a2fc3cddf29bdc75
jorag <jorgen.agersborg@uit.no>
2018-08-31 16:43:37 +0200

    More code cleanup and comments

--- geopixpos.py
+++ geopixpos.py
@@ -33,3 +33,42 @@ def pos2pix(geotransform, lat='default', lon='default', verbose=False):
         
     # Return pixel position
     return pixpos_lat, pixpos_lon
+
+
+def refinepos(a):
+    """TODO: Implement this!
+    
+    Idea: Use the Geo-transform info to get a rough estimate of position, then look up "exact" position using LAT/LONG bands
+    Look at sign of band minus coordinate of interest
+    Use mean(abs(diff)) as sanity check, shuld be much smaller than 1 if correct (lat/long) band
+    
+    Needs GDAL (import). 
+    Needs numpy as np
+    Needs indice of lat/long bands.
+    """
+    lat_band = dataset.GetRasterBand(lat_band_i)
+    print("Band Type={}".format(gdal.GetDataTypeName(lat_band.DataType)))
+    
+    # Offset in pixels from estimated position so that the estimated position is at the centre
+    # Read this many extra pixels in each direction
+    pix_offset_x = 2
+    pix_offset_y = 2
+    
+    # Read geoposition band for fine search of position
+    data = lat_band.ReadAsArray(pixpos_lon - pix_offset_x, pixpos_lat - pix_offset_y, pix_offset_x*2+1, pix_offset_y*2+1)
+    #data = lat_band.ReadAsArray(int(exact_pixpos_lon), int(exact_pixpos_lat), 3, 3)
+    print(data)
+    
+    diff_data = data-point_lat
+    abs_diff = abs(diff_data)
+    print(diff_data)
+    a = np.where(abs_diff == np.min(abs_diff))
+    b = np.argmin(abs_diff)
+    
+    # In the original example, there point of interests latitude is equal distance between 69.99997 lat and 70.00003 lat
+    # In this case, there should be no refinement to the original estimate
+    # Or should perhaps both be considered?
+    
+    print(data[a]) 
+    # Does not work for b - this is because of how armin works in Python:
+    # In case of multiple occurrences of the minimum values, the indices corresponding to the first occurrence are returned.
