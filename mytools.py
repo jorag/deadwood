368ccc1b820d00fb86a65cd8b9db48d1888de8d2
jorag <jorgen.agersborg@uit.no>
2018-09-13 21:35:46 +0200

    Created mytools.py for various useful general functions.
    E.g. a function for logging/printing and a function that mimics MATLAB's length (thanks MadsAdrian!).

---
+++ mytools.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 10 16:17:30 2018
+
+@author: jorag
+@author of length: MadsAdrian, updated by jorag 
+"""
+
+import numpy as np
+
+def length(x):
+    """Returns length of input.
+    
+    Mimics MATLAB's length function.
+    """
+	if isinstance(x,(int,float,complex)):
+		return 1
+	elif isinstance(x,np.ndarray):
+		return max(x.shape)
+	try:
+		return len(x)
+	except TypeError as e:
+		print('In length, add handling for type ' + str(type(x)))
+		raise e
+        
+        
+def mynormal(x, params):
+    """Univariate normal probability density function.
+    
+    Input:
+        x - point(s)
+        params - contains mu and sigma (in that order)
+    Output:
+        pdf(x)
+    """
+    mu = params[0]
+    sigma = params[1]
+    
+    pdf = 1/(np.sqrt(2*np.pi) * sigma) * np.exp(-(x - mu)**2/(2*sigma**2))
+    return pdf
+
+
+def logit(txt_in, log_type='print'):
+    """Function for logging text to file or printing to console.
+    
+    Enables a unified command for printing text during development/debugging
+    and logging the text to a file during normal (mature) operations.
+    """
+    if log_type == 'print':
+        print(txt_in)
+        
+    return
