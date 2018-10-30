4a705cb7d0737a51ce613da2da8d287396bc961d
jorag <jorgen.agersborg@uit.no>
2018-10-30 17:31:16 +0100

    Continiue transitioning from using data list to data object

--- get_sat_data.py
+++ get_sat_data.py
@@ -68,6 +68,43 @@ showimpoint(all_sat_bands, geotransform, point_lat, point_lon, n_pixel_x=500, n_
 # Show entire image
 showimage(all_sat_bands, bands=[0,1,2])
     
+
+# Read Excel file with vegetation types
+try:
+    # Read predefined file
+    with open(os.path.join(dirname, "data", "vegetation-data-path")) as infile:
+        veg_file = infile.readline().strip()
+        logit('Read file: ' + veg_file, log_type = 'default')
+    
+    # Load data
+    xls = pd.ExcelFile(veg_file)
+except:      
+    logit('Error, promt user for file.', log_type = 'default')
+    # Predefined file failed for some reason, promt user
+    root = tkinter.Tk() # GUI for file selection
+    root.withdraw()
+    veg_file = tkinter.filedialog.askopenfilename(title='Select input .csv/.xls(x) file')
+    xls = pd.ExcelFile(veg_file)
+
+
+# Go through all sheets in Excel sheet
+point_info = []
+class_init = []
+name_init = []
+for i_sheet in range(1,7):
+    print(i_sheet)
+    # Get pandas dataframe
+    df = pd.read_excel(xls, str(i_sheet))
+    point_id = list(df['GPSwaypoint'])
+    # Go through the list of points
+    for id in point_id:
+        #print(id)
+        #print(gps_id.index(id), df['LCT1_2017'][point_id.index(id)], pos_array[gps_id.index(id)])
+        #point_info.append([gps_id.index(id), df['LCT1_2017'][point_id.index(id)], pos_array[gps_id.index(id)]])
+        name_init.append(id)
+        class_init.append(df['LCT1_2017'][point_id.index(id)])
+
+
 # Read .gpx file with coordinates of transect points
 try:
     # Read predefined file
@@ -102,27 +139,7 @@ gps_points = list(zip(gps_id, pos_array))
 #pos_array2 = np.asarray(pos_array)
 pos_array2 = np.asarray([item[1] for item in gps_points])
 gps_id2 = [item[0] for item in gps_points]
-
-
-# Read Excel file with vegetation types
-try:
-    # Read predefined file
-    with open(os.path.join(dirname, "data", "vegetation-data-path")) as infile:
-        veg_file = infile.readline().strip()
-        logit('Read file: ' + veg_file, log_type = 'default')
-    
-    # Load data
-    xls = pd.ExcelFile(veg_file)
-except:      
-    logit('Error, promt user for file.', log_type = 'default')
-    # Predefined file failed for some reason, promt user
-    root = tkinter.Tk() # GUI for file selection
-    root.withdraw()
-    veg_file = tkinter.filedialog.askopenfilename(title='Select input .csv/.xls(x) file')
-    xls = pd.ExcelFile(veg_file)
-
-
-   
+       
 # Categorize points
 
 
@@ -131,31 +148,13 @@ pix_lat, pix_long = pos2pix(geotransform, lon=pos_array2[:,1], lat=pos_array2[:,
 
 # Extract pixels from area
 data_out = all_sat_bands[0:3, pix_lat, pix_long]
-
-# Go through all sheets in Excel sheet
-point_info = []
-class_init = []
-name_init = []
-for i_sheet in range(1,7):
-    print(i_sheet)
-    # Get pandas dataframe
-    df = pd.read_excel(xls, str(i_sheet))
-    point_id = list(df['GPSwaypoint'])
-    # Go through the list of points
-    for id in point_id:
-        #print(id)
-        #print(gps_id.index(id), df['LCT1_2017'][point_id.index(id)], pos_array[gps_id.index(id)])
-        point_info.append([gps_id.index(id), df['LCT1_2017'][point_id.index(id)], pos_array[gps_id.index(id)]])
-        name_init.append(id)
-        class_init.append(df['LCT1_2017'][point_id.index(id)])
-
-        
-        
         
 # Intialize data object
 all_data = DataModalities('Polmak')
 # Add points
 all_data.add_points(name_init, class_init)
+# Add GPS points
+all_data.add_meta(gps_id, 'gps_coordinates', pos_array2)
 
 # Add meta values to some points
 #all_data.add_meta(['N_6_155', 'N_6_156'], 'testmeta', [99999, '35732475793245 b3480534'])
