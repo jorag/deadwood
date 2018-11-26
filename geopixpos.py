a84ac9bc1b62d6d758d6b77725168518fdeada5a
jorag <jorgen.agersborg@uit.no>
2018-11-26 15:35:45 +0100

    Add Haversine function to geopixpos module for possible future use
    Python Haversine implementation copied from StackOverflow answer:
    https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points

--- geopixpos.py
+++ geopixpos.py
@@ -219,3 +219,23 @@ def bandinfo_log(band, log_type='default'):
     if band.GetRasterColorTable():
         logit("Band, number of colour table with entries:", log_type)
         logit(band.GetRasterColorTable().GetCount(), log_type)
+        
+        
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    
+    From: https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
+    """
+    from math import radians, cos, sin, asin, sqrt
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
