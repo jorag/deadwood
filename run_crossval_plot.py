0149c919139734545d1fb963a180ad2028db4c9c
jorag <jorgen.agersborg@uit.no>
2019-01-10 11:40:55 +0100

    Rewrite run_crossval_plot to loop over all stored results

--- run_crossval_plot.py
+++ run_crossval_plot.py
@@ -28,9 +28,9 @@ datamod_fprefix = ''
 crossval_fprefix = ''
 
 # Name of input object and file with satellite data path string
-dataset_use = 'vanZyl-B'
-obj_in_name = datamod_fprefix + dataset_use + '.pkl'
-sat_pathfile_name = dataset_use + '-path'
+#dataset_use = 'vanZyl-B'
+#obj_in_name = datamod_fprefix + dataset_use + '.pkl'
+#sat_pathfile_name = dataset_use + '-path'
 
 # Path to working directory 
 dirname = os.path.realpath('.') # For parent directory use '..'
@@ -48,44 +48,56 @@ with open(os.path.join(dirname, 'data', knn_file ), 'rb') as infile:
 with open(os.path.join(dirname, 'data', rf_file ), 'rb') as infile:
     rf_cv_all, rf_cv_sar, rf_cv_opt = pickle.load(infile)
     
+# List of datasets to process
+dataset_list = ['Coh-A', 'Coh-B', 'Coh-C', 'vanZyl-A', 'vanZyl-B', 'vanZyl-C']
+# dataset_list = ['vanZyl-A']
 
-# GET CROSS-VALIDATE RESULTS
+knn_all_means = []
+knn_all_stds = []
+knn_all_idx = []
+idx_data = 0
 
-# Set class labels for dictionary - TODO: Consider moving this to get_stat_data
-class_dict_in = dict([['Live', 1], ['Defoliated', 2], ['other', 0]])
-
-
-# Cross validate - kNN - All data
-knn_scores_all = knn_cv_all[dataset_use]
-print('kNN OPT+SAR - ' + dataset_use + ' :')
-print(knn_scores_all) 
-
-# Cross validate - kNN - SAR data
-knn_scores_sar = knn_cv_sar[dataset_use]
-print('kNN SAR only - ' + dataset_use + ' :')
-print(knn_scores_sar)
-
-# Cross validate - kNN - OPT data
-knn_scores_opt = knn_cv_opt[dataset_use]
-print('kNN opt only - ' + dataset_use + ' :')
-print(knn_scores_opt) 
-
-
-# Cross validate - Random Forest - All data
-rf_scores_all = rf_cv_all[dataset_use]
-print('RF OPT+SAR - ' + dataset_use + ' :')
-print(rf_scores_all) 
-
-# Cross validate - Random Forest - SAR data
-rf_scores_sar = rf_cv_sar[dataset_use]
-print('RF OPT+SAR - ' + dataset_use + ' :')
-print(rf_scores_sar)
-
-# Cross validate - Random Forest - OPT data
-rf_scores_opt = rf_cv_opt[dataset_use]
-print('RF OPT - ' + dataset_use + ' :')
-print(rf_scores_opt)         
+# Loop through all satellite images
+for dataset_use in dataset_list:
+    idx_data += 1
+    # GET CROSS-VALIDATE RESULTS
+    
+    # Cross validate - kNN - All data
+    knn_scores_all = knn_cv_all[dataset_use]
+    print('kNN OPT+SAR - ' + dataset_use + ' :')
+    print(knn_scores_all) 
+    # Add to list
+    knn_all_means.append(np.mean(knn_scores_all))
+    knn_all_stds.append(np.std(knn_scores_all))
+    knn_all_idx.append(idx_data)
+    
+    # Cross validate - kNN - SAR data
+    knn_scores_sar = knn_cv_sar[dataset_use]
+    print('kNN SAR only - ' + dataset_use + ' :')
+    print(knn_scores_sar)
+    
+    # Cross validate - kNN - OPT data
+    knn_scores_opt = knn_cv_opt[dataset_use]
+    print('kNN opt only - ' + dataset_use + ' :')
+    print(knn_scores_opt) 
+    
+    
+    # Cross validate - Random Forest - All data
+    rf_scores_all = rf_cv_all[dataset_use]
+    print('RF OPT+SAR - ' + dataset_use + ' :')
+    print(rf_scores_all) 
+    
+    # Cross validate - Random Forest - SAR data
+    rf_scores_sar = rf_cv_sar[dataset_use]
+    print('RF OPT+SAR - ' + dataset_use + ' :')
+    print(rf_scores_sar)
+    
+    # Cross validate - Random Forest - OPT data
+    rf_scores_opt = rf_cv_opt[dataset_use]
+    print('RF OPT - ' + dataset_use + ' :')
+    print(rf_scores_opt)
+     
 
 fig = plt.figure()
-plt.errorbar([1,2], [np.mean(knn_scores_all), np.mean(knn_scores_sar)], [np.std(knn_scores_all), np.std(knn_scores_sar)], linestyle='None', marker='^')
+plt.errorbar(knn_all_idx, knn_all_means, knn_all_stds, linestyle='None', marker='^', capsize=3)
 plt.show()
