04e2eba02b429383c77b1644f0341e6039afc7a7
jorag <jorgen.agersborg@uit.no>
2019-09-19 18:44:25 +0200

    Loop over all modalities in object, plot, and calculate performance

--- run_backscatter_plot.py
+++ run_backscatter_plot.py
@@ -29,7 +29,6 @@ datamod_fprefix = 'Sept1-19'
 dataset_id = 'C'
 
 dataset_use = 'PGNLM3'
-#dataset_use = 'Coh-A'
 
 # Name of input object and file with satellite data path string
 obj_in_name = datamod_fprefix + '-' + dataset_id + '.pkl'
@@ -56,74 +55,75 @@ for i_var_y in range(n_var_y):
 
 #trees = all_data.read_data_points('Tree') # Fails due to to all points having trees and hence no "Tree" attribute
 
-# Get SAR data 
-sar_data = all_data.read_data_points(dataset_use+'-'+dataset_id, modality_type=dataset_use) 
-# Get OPT data
-opt_data = all_data.read_data_points(dataset_use+'-'+dataset_id, modality_type='optical') 
-
-# Remove singelton dimensions
-sar_data = np.squeeze(sar_data)
-opt_data = np.squeeze(opt_data)
-
-
-# Canonical-correlation analysis (CCA)
-n_comp = 2
-cca = CCA(n_components=n_comp)
-cca.fit(sar_data, y_data)
-
-# Get CCA transformation
-U_c, V_c  = cca.x_scores_, cca.y_scores_#= cca.transform(sar_data, y_data)
-
-# From: https://stackoverflow.com/questions/37398856/
-rho_cca = np.corrcoef(U_c.T, V_c.T).diagonal(offset=n_comp)
-#score = np.diag(np.corrcoef(cca.x_scores_, cca.y_scores_, rowvar=False)[:n_comp, n_comp:])
-
-# Calculate Coefficient of Determination (COD) = R²
-cod_cca = cca.score(sar_data, y_data)
-print(cod_cca)
-
-# Plot number of trees vs. backscatter values
-c_vec = mycolourvec()
-legend_list = []
-fig = plt.figure()
-for i_comp in range(n_comp):
-    plt.scatter(U_c[:,i_comp], V_c[:,i_comp], c=c_vec[i_comp])
-    legend_list.append('Comp. nr. '+str(i_comp)+ r' $\rho$ = ' +'{:.3f}'.format(rho_cca[i_comp]))
-plt.title('CCA: R^2 = ' +'{:.3f}'.format(cod_cca))
-plt.legend(legend_list)
-plt.show()  # display it
-
-
-# Plot multivariate linear regression for PLC and PDC
-# Set transformation
-transformation = identity # np.log # Loop over transformations?
-for response in ['plc', 'pdc']:
-    r = y_data[:, y_var_read.index(response)]  # Response
-    r = transformation(r) # Do a transformation
-    r[~np.isfinite(r)] = -100 # Replace NaNs 
-    # Add column of ones
-    X = np.hstack((np.ones((length(r),1)), sar_data))
-    # Find regression parameters
-    W = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.T,X)),X.T), r)
-    # Find predicted curve
-    reg_curve = np.matmul(X,W)
-    # Calculate Coefficient of Determination (COD) = R²
-    cod_reg = rsquare(r, reg_curve)
-    print(cod_reg)
+# Analyse all data modalities in object
+for dataset_use in all_data.all_modalities:
+    # Calculate measures of performance for CCA and linear regression
     
-    # Create figure
+    if dataset_use.lower() in ['optical']:
+        continue
+                                   
+    # Get SAR data 
+    sar_data = all_data.read_data_points(dataset_use+'-'+dataset_id, modality_type=dataset_use) 
+    # Get OPT data
+    #opt_data = all_data.read_data_points(dataset_use+'-'+dataset_id, modality_type='optical') 
+    
+    # Remove singelton dimensions
+    sar_data = np.squeeze(sar_data)
+    #opt_data = np.squeeze(opt_data)
+    
+    
+    # Canonical-correlation analysis (CCA)
+    n_comp = 2
+    cca = CCA(n_components=n_comp)
+    cca.fit(sar_data, y_data)
+    
+    # Get CCA transformation
+    U_c, V_c  = cca.x_scores_, cca.y_scores_#= cca.transform(sar_data, y_data)
+    
+    # From: https://stackoverflow.com/questions/37398856/
+    rho_cca = np.corrcoef(U_c.T, V_c.T).diagonal(offset=n_comp)
+    #score = np.diag(np.corrcoef(cca.x_scores_, cca.y_scores_, rowvar=False)[:n_comp, n_comp:])
+    
+    # Calculate Coefficient of Determination (COD) = R²
+    cod_cca = cca.score(sar_data, y_data)
+    print(cod_cca)
+    
+    # Plot number of trees vs. backscatter values
+    c_vec = mycolourvec()
+    legend_list = []
     fig = plt.figure()
-    # Plot data
-    plt.plot(r, color='b')
-    plt.plot(reg_curve, color='r')
-    plt.legend(['Measured '+response.upper(), 'Regression '+response.upper()])
-    plt.xlabel('Transect point nr.')
-    plt.ylabel(response.upper())
-    plt.title('Multivariate/multiple linear regression: R^2 = ' +'{:.3f}'.format(cod_reg))
-    plt.show()  # display 
-
-
-# For analysing all datasets in object
-
-# for dataset_use in dataset_list:
-    # Calculate single number
+    for i_comp in range(n_comp):
+        plt.scatter(U_c[:,i_comp], V_c[:,i_comp], c=c_vec[i_comp])
+        legend_list.append('Comp. nr. '+str(i_comp)+ r' $\rho$ = ' +'{:.3f}'.format(rho_cca[i_comp]))
+    plt.title('CCA: R^2 = ' +'{:.3f}'.format(cod_cca))
+    plt.legend(legend_list)
+    plt.show()  # display it
+    
+    
+    # Plot multivariate linear regression for PLC and PDC
+    # Set transformation
+    transformation = identity # np.log # Loop over transformations?
+    for response in ['plc', 'pdc']:
+        r = y_data[:, y_var_read.index(response)]  # Response
+        r = transformation(r) # Do a transformation
+        r[~np.isfinite(r)] = -100 # Replace NaNs 
+        # Add column of ones
+        X = np.hstack((np.ones((length(r),1)), sar_data))
+        # Find regression parameters
+        W = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.T,X)),X.T), r)
+        # Find predicted curve
+        reg_curve = np.matmul(X,W)
+        # Calculate Coefficient of Determination (COD) = R²
+        cod_reg = rsquare(r, reg_curve)
+        print(cod_reg)
+        
+        # Create figure
+        fig = plt.figure()
+        # Plot data
+        plt.plot(r, color='b')
+        plt.plot(reg_curve, color='r')
+        plt.legend(['Measured '+response.upper(), 'Regression '+response.upper()])
+        plt.xlabel('Transect point nr.')
+        plt.ylabel(response.upper())
+        plt.title('Multivariate/multiple linear regression: R^2 = ' +'{:.3f}'.format(cod_reg))
+        plt.show()  # display 
