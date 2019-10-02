dfc13e13b8c493193e883a2ef55217cbd5cce2a9
jorag <jorgen.agersborg@uit.no>
2019-10-02 17:59:15 +0200

    Modify code to run over all datasets (using IDs "A", "B", "C")
    Plot resulting R^2 as bar plots.

--- run_backscatter_plot.py
+++ run_backscatter_plot.py
@@ -26,14 +26,14 @@ dirname = os.path.realpath('.') # For parent directory use '..'
 
 # Prefix for object filename
 datamod_fprefix = 'All-data-0919'
-dataset_id = 'A'
+id_list = ['A', 'B', 'C']
 
 # Name of input object and file with satellite data path string
 #obj_in_name = datamod_fprefix + '-' + dataset_id + '.pkl'
 obj_in_name = datamod_fprefix + '-' + '.pkl'
 
 # List of plots
-plot_list = ['cca_reg'] # ['cca', 'pxcvsu', 'linreg', 'cca_reg']
+plot_list = [] # ['cca', 'pxcvsu', 'linreg', 'cca_reg']
 
 # Parameters
 y_var_read = ['plc', 'pdc']
@@ -84,131 +84,133 @@ cca_plc_r2 = dict()
 cca_pdc_r2 = dict()
 
 # Analyse all data modalities in object
-# Calculate measures of performance for CCA and linear regression   
-for dataset_type in all_data.all_modalities:
-    print(dataset_type)            
-    # Get satellite data
-    try:
-        dataset_use = dataset_type+'-'+dataset_id
-        sat_data = all_data.read_data_points(dataset_use, modality_type=dataset_type)
-        print(dataset_use)
-    except:
-        continue
-    
-    # Ensure that the output array has the proper shape (2 dimensions)
-    if length(sat_data.shape) == 1:
-        # If data is only a single column make it a proper vector
-        sat_data = sat_data[:, np.newaxis]
-    elif length(sat_data.shape) > 2:
-        # Remove singelton dimensions
-        sat_data = np.squeeze(sat_data)
-    #print(sat_data)
-    
-    # TODO: 20190930 - assert that n_cca_comp >= sat_data.shape[1] ?
-    # For now ignore NDVI
-    if n_cca_comp > sat_data.shape[1]:
-        continue
-    
-    # Canonical-correlation analysis (CCA)
-    cca = CCA(n_components=n_cca_comp)
-    cca.fit(sat_data, y_data)
-    
-    # Get CCA transformation
-    U_c, V_c  = cca.x_scores_, cca.y_scores_ #= cca.transform(sat_data, y_data)
-    
-    # From: https://stackoverflow.com/questions/37398856/
-    rho_cca = np.corrcoef(U_c.T, V_c.T).diagonal(offset=n_cca_comp)
-    #score = np.diag(np.corrcoef(cca.x_scores_, cca.y_scores_, rowvar=False)[:n_cca_comp, n_cca_comp:])
-    
-    # Use function definition
-    cod_cca2 = rsquare(U_c, y_data)
-    print(cod_cca2)
-    # Add to output dict
-    cca_plc_r2[dataset_use] = cod_cca2[0] # TODO: set index programatically
-    cca_pdc_r2[dataset_use] = cod_cca2[1] # TODO: set index programatically
-    # Calculate Coefficient of Determination (COD) = R²
-    cod_cca = cca.score(sat_data, y_data)
-    print(cod_cca)
-
-    # Plot number of CCA U and V
-    if 'CCA'.lower() in plot_list:
-        legend_list = []
-        fig = plt.figure()
-        for i_comp in range(n_cca_comp):
-            plt.scatter(U_c[:,i_comp], V_c[:,i_comp], c=c_vec[i_comp])
-            legend_list.append('Comp. nr. '+str(i_comp)+ r' $\rho$ = ' +'{:.3f}'.format(rho_cca[i_comp]))
-        plt.title(dataset_use+' CCA: R^2 = ' +'{:.3f}'.format(cod_cca))
-        plt.legend(legend_list)
-        plt.show()  # display it
-    
-    # Plot number of CCA U and PLC
-    if 'PxCvsU'.lower() in plot_list:
-        legend_list = []
-        fig = plt.figure()
-        for i_comp in range(n_cca_comp):
-            plt.scatter(V_c[:,i_comp], y_data[:, y_var_read.index('plc')] , c=c_vec[i_comp])
-            legend_list.append('Comp. nr. '+str(i_comp)+ r' $\rho$ = ' +'{:.3f}'.format(rho_cca[i_comp]))
-        plt.title(dataset_use+' CCA: R^2 = ' +'{:.3f}'.format(cod_cca))
-        plt.xlabel('U'); plt.ylabel('PLC'); plt.legend(legend_list)
-        plt.show()
+# Calculate measures of performance for CCA and linear regression
+# Loop through all satellite images
+for dataset_id in id_list: 
+    for dataset_type in all_data.all_modalities:
+        print(dataset_type)            
+        # Get satellite data
+        try:
+            dataset_use = dataset_type+'-'+dataset_id
+            sat_data = all_data.read_data_points(dataset_use, modality_type=dataset_type)
+            print(dataset_use)
+        except:
+            continue
         
-        # Plot number of CCA U and PDC
-        legend_list = []
-        fig = plt.figure()
-        for i_comp in range(n_cca_comp):
-            plt.scatter(V_c[:,i_comp], y_data[:, y_var_read.index('pdc')] , c=c_vec[i_comp])
-            legend_list.append('Comp. nr. '+str(i_comp)+ r' $\rho$ = ' +'{:.3f}'.format(rho_cca[i_comp]))
-        plt.title(dataset_use+' CCA: R^2 = ' +'{:.3f}'.format(cod_cca))
-        plt.xlabel('U'); plt.ylabel('PDC'); plt.legend(legend_list)
-        plt.show()  # display it
+        # Ensure that the output array has the proper shape (2 dimensions)
+        if length(sat_data.shape) == 1:
+            # If data is only a single column make it a proper vector
+            sat_data = sat_data[:, np.newaxis]
+        elif length(sat_data.shape) > 2:
+            # Remove singelton dimensions
+            sat_data = np.squeeze(sat_data)
+        #print(sat_data)
         
-    # Plot number of CCA U and PLC
-    if 'CCA_reg'.lower() in plot_list:
-        X_CCA = np.hstack((np.ones((sat_data.shape[0],1)), U_c))
-        # Find regression parameters and regression curve
-        W_CCA = np.matmul(np.matmul(np.linalg.inv(np.matmul(X_CCA.T,X_CCA)),X_CCA.T), y_data)
-        r_CCA = np.matmul(X_CCA, W_CCA)
+        # TODO: 20190930 - assert that n_cca_comp >= sat_data.shape[1] ?
+        # For now ignore NDVI
+        if n_cca_comp > sat_data.shape[1]:
+            continue
+        
+        # Canonical-correlation analysis (CCA)
+        cca = CCA(n_components=n_cca_comp)
+        cca.fit(sat_data, y_data)
+        
+        # Get CCA transformation
+        U_c, V_c  = cca.x_scores_, cca.y_scores_ #= cca.transform(sat_data, y_data)
+        
+        # From: https://stackoverflow.com/questions/37398856/
+        rho_cca = np.corrcoef(U_c.T, V_c.T).diagonal(offset=n_cca_comp)
+        #score = np.diag(np.corrcoef(cca.x_scores_, cca.y_scores_, rowvar=False)[:n_cca_comp, n_cca_comp:])
+        
+        # Use function definition
+        cod_cca2 = rsquare(U_c, y_data)
+        print(cod_cca2)
+        # Add to output dict
+        cca_plc_r2[dataset_use] = cod_cca2[0] # TODO: set index programatically
+        cca_pdc_r2[dataset_use] = cod_cca2[1] # TODO: set index programatically
         # Calculate Coefficient of Determination (COD) = R²
-        cca_reg = rsquare(y_data, r_CCA)
-        print(r'CCA R^2')
-        print(cca_reg)
-        for i_col in range(r_CCA.shape[1]):
-            fig = plt.figure()
-            # Plot data
-            plt.plot(y_data[:, i_col] , color='b')
-            plt.plot(r_CCA[:, i_col], color='r')
-            plt.legend(['Measured '+y_var_read[i_col].upper(), 'Regression '+y_var_read[i_col].upper()])
-            plt.xlabel('Transect point nr.')
-            plt.ylabel(y_var_read[i_col].upper())
-            plt.title(dataset_use+' CCA regression: R^2 = ' +'{:.3f}'.format(cca_reg[i_col]))
-            plt.show()  # display 
-        
-    # Linreg - TODO: 20190929 - Do some transformation??
-    # Add column of ones
-    X = np.hstack((np.ones((sat_data.shape[0],1)), sat_data))
-    # Find regression parameters
-    W = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.T,X)),X.T), y_data)
-    r_hat = np.matmul(X,W)
-    # Calculate Coefficient of Determination (COD) = R²
-    cod_reg = rsquare(y_data, r_hat)
-    print(r'Regression R^2')
-    print(cod_reg)
+        cod_cca = cca.score(sat_data, y_data)
+        print(cod_cca)
     
-    # Add to output dict
-    linreg_plc_r2[dataset_use] = cod_reg[0] # TODO: set index programatically
-    linreg_pdc_r2[dataset_use] = cod_reg[1] # TODO: set index programatically
-    # Plot Multivariate/multiple linear linear regression for PLC and PDC
-    if 'linreg' in plot_list:
-        for i_col in range(r_hat.shape[1]):
+        # Plot number of CCA U and V
+        if 'CCA'.lower() in plot_list:
+            legend_list = []
             fig = plt.figure()
-            # Plot data
-            plt.plot(y_data[:, i_col] , color='b')
-            plt.plot(r_hat[:, i_col], color='r')
-            plt.legend(['Measured '+y_var_read[i_col].upper(), 'Regression '+y_var_read[i_col].upper()])
-            plt.xlabel('Transect point nr.')
-            plt.ylabel(y_var_read[i_col].upper())
-            plt.title(dataset_use+' regression: R^2 = ' +'{:.3f}'.format(cod_reg[i_col]))
-            plt.show()  # display 
+            for i_comp in range(n_cca_comp):
+                plt.scatter(U_c[:,i_comp], V_c[:,i_comp], c=c_vec[i_comp])
+                legend_list.append('Comp. nr. '+str(i_comp)+ r' $\rho$ = ' +'{:.3f}'.format(rho_cca[i_comp]))
+            plt.title(dataset_use+' CCA: R^2 = ' +'{:.3f}'.format(cod_cca))
+            plt.legend(legend_list)
+            plt.show()  # display it
+        
+        # Plot number of CCA U and PLC
+        if 'PxCvsU'.lower() in plot_list:
+            legend_list = []
+            fig = plt.figure()
+            for i_comp in range(n_cca_comp):
+                plt.scatter(V_c[:,i_comp], y_data[:, y_var_read.index('plc')] , c=c_vec[i_comp])
+                legend_list.append('Comp. nr. '+str(i_comp)+ r' $\rho$ = ' +'{:.3f}'.format(rho_cca[i_comp]))
+            plt.title(dataset_use+' CCA: R^2 = ' +'{:.3f}'.format(cod_cca))
+            plt.xlabel('U'); plt.ylabel('PLC'); plt.legend(legend_list)
+            plt.show()
+            
+            # Plot number of CCA U and PDC
+            legend_list = []
+            fig = plt.figure()
+            for i_comp in range(n_cca_comp):
+                plt.scatter(V_c[:,i_comp], y_data[:, y_var_read.index('pdc')] , c=c_vec[i_comp])
+                legend_list.append('Comp. nr. '+str(i_comp)+ r' $\rho$ = ' +'{:.3f}'.format(rho_cca[i_comp]))
+            plt.title(dataset_use+' CCA: R^2 = ' +'{:.3f}'.format(cod_cca))
+            plt.xlabel('U'); plt.ylabel('PDC'); plt.legend(legend_list)
+            plt.show()  # display it
+            
+        # Plot number of CCA U and PLC
+        if 'CCA_reg'.lower() in plot_list:
+            X_CCA = np.hstack((np.ones((sat_data.shape[0],1)), U_c))
+            # Find regression parameters and regression curve
+            W_CCA = np.matmul(np.matmul(np.linalg.inv(np.matmul(X_CCA.T,X_CCA)),X_CCA.T), y_data)
+            r_CCA = np.matmul(X_CCA, W_CCA)
+            # Calculate Coefficient of Determination (COD) = R²
+            cca_reg = rsquare(y_data, r_CCA)
+            print(r'CCA R^2')
+            print(cca_reg)
+            for i_col in range(r_CCA.shape[1]):
+                fig = plt.figure()
+                # Plot data
+                plt.plot(y_data[:, i_col] , color='b')
+                plt.plot(r_CCA[:, i_col], color='r')
+                plt.legend(['Measured '+y_var_read[i_col].upper(), 'Regression '+y_var_read[i_col].upper()])
+                plt.xlabel('Transect point nr.')
+                plt.ylabel(y_var_read[i_col].upper())
+                plt.title(dataset_use+' CCA regression: R^2 = ' +'{:.3f}'.format(cca_reg[i_col]))
+                plt.show()  # display 
+            
+        # Linreg - TODO: 20190929 - Do some transformation??
+        # Add column of ones
+        X = np.hstack((np.ones((sat_data.shape[0],1)), sat_data))
+        # Find regression parameters
+        W = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.T,X)),X.T), y_data)
+        r_hat = np.matmul(X,W)
+        # Calculate Coefficient of Determination (COD) = R²
+        cod_reg = rsquare(y_data, r_hat)
+        print(r'Regression R^2')
+        print(cod_reg)
+        
+        # Add to output dict
+        linreg_plc_r2[dataset_use] = cod_reg[0] # TODO: set index programatically
+        linreg_pdc_r2[dataset_use] = cod_reg[1] # TODO: set index programatically
+        # Plot Multivariate/multiple linear linear regression for PLC and PDC
+        if 'linreg' in plot_list:
+            for i_col in range(r_hat.shape[1]):
+                fig = plt.figure()
+                # Plot data
+                plt.plot(y_data[:, i_col] , color='b')
+                plt.plot(r_hat[:, i_col], color='r')
+                plt.legend(['Measured '+y_var_read[i_col].upper(), 'Regression '+y_var_read[i_col].upper()])
+                plt.xlabel('Transect point nr.')
+                plt.ylabel(y_var_read[i_col].upper())
+                plt.title(dataset_use+' regression: R^2 = ' +'{:.3f}'.format(cod_reg[i_col]))
+                plt.show()  # display 
 
 
 # Plot summary statistics
