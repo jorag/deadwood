7fad0251e485c46a27deaeed95429b33d5891b82
jorag <jorgen.agersborg@uit.no>
2019-11-29 14:46:50 +0100

    Create initial dialogue implementation

--- create_band_info.py
+++ create_band_info.py
@@ -46,10 +46,47 @@ raster_data_array = dataset.ReadAsArray()
 # - ask user if he wants to add band to a list (lat, lon, SAR, or opt), multiple choise
 
                                                
-for i_band in range(2): #range(raster_data_array.shape[0]):
-    plt.figure()
-    plt.imshow(raster_data_array[i_band,:,:]) 
-    plt.show()  # display it
+#for i_band in range(raster_data_array.shape[0]):
+#    plt.figure()
+#    plt.imshow(raster_data_array[i_band,:,:]) 
+#    plt.show()  # display it
+             
+# Initialize output lists
+sorting_dict = {'sar_bands': [], 'opt_bands': [], 'lat_band': [], 
+'lon_band': [], 'unsure': [], 'none': []}
+#sar_bands = []             
+#opt_bands = []
+#lat_band = []
+#lon_band = []
+
+# List for question dialogue
+
+
+# Rearrage dimensions to x,y,channel format
+im_generate = np.transpose(raster_data_array, (1,2,0))
+    
+for i_band in range(im_generate.shape[2]-20):
+    # Get statistics
+    band_min = np.nanmin(im_generate[:,:,i_band])
+    band_mean = np.nanmean(im_generate[:,:,i_band])
+    band_max = np.nanmax(im_generate[:,:,i_band])
+    # Band title text
+    band_txt = 'BAND '+ str(i_band) 
+    plt.figure(i_band)
+    plt.imshow(im_generate[:,:,i_band])
+    plt.title(band_txt +' Min = '+str(band_min)+' Mean = '+str(band_mean)+' Max = '+str(band_max))
+    plt.pause(0.05)
+    #plt.show()  # display it
+    
+    # Ask which band it its
+    which_list = ask_multiple_choice_question('Which band type is this?', list(sorting_dict.keys()), title=band_txt)
+    print("User's response was: {}".format(repr(which_list)))
+    print(which_list)
+    
+    # Store result
+    sorting_dict[which_list].append(i_band)
+    
+    plt.close(i_band)
     
 # Promt user and ask if band list should be stored
 # - if there are already band lists, show the current list and the pre-existing one
