6af8df11b3e6871e9af62614788c5446d2b02a52
jorag <jorgen.agersborg@uit.no>
2020-01-06 18:09:42 +0100

    Find the "best" dataset, print it and add it to max accuracy plot
    TODO: Only for kNN for now, implement it for RF (and perhaps SVM).
    TODO: Clean-up, "move do_plot_flags" etc.

--- run_gridsearch_plot.py
+++ run_gridsearch_plot.py
@@ -74,6 +74,77 @@ if do_cross_set:
     best_svm_xset_gain = np.asarray(xset_svm_max) - np.asarray(largest_class_size)
 
 
+#%% Summary of Random Forest (rf) results
+plot_dataset_rf = True
+if plot_dataset_rf:
+    # Get list of datasets
+    dataset_keys = result_rf_cross_val[0].keys() # TODO: consider storing dataset keys in result_summary or other variable
+    dataset_list = list(dataset_keys)
+    n_sets = length(dataset_keys)
+    rf_result_vec = np.zeros((n_sets ,1))
+    rf_best_set_vec = np.zeros((n_sets ,1))
+    for i_run, result_dict in enumerate(result_rf_cross_val):
+        # Get best result
+        best_res = result_summary[i_run]['cross-val_rf_max']
+        for i_dataset, key in enumerate(result_dict):
+            # Find diff from best result
+            rf_result_vec[i_dataset, 0] += result_dict[key] - best_res
+            rf_best_set_vec[i_dataset, 0] += 1-np.ceil(best_res-result_dict[key])
+    
+    # Diff                  
+    fig = plt.figure()
+    plt.title('RF dataset result, diff from max accuracy')
+    plt.scatter(np.arange(n_sets), rf_result_vec, c='r', marker='o')
+    plt.xlabel('Dataset nr.')
+    plt.show()
+    # Count number of best results
+    fig = plt.figure()
+    plt.title('Number of best accuracy results, RF')
+    plt.scatter(np.arange(n_sets), rf_best_set_vec, c='r', marker='o')
+    plt.xlabel('Dataset nr.')
+    plt.show()
+    
+    # Find best set
+    knn_best_diff = dataset_list[np.argmax(rf_result_vec)]
+    knn_best_vote = dataset_list[np.argmax(rf_best_set_vec)]
+    print('kNN, best average deviation from max accuracy: ' + knn_best_diff)
+    print('kNN, most max accuracy results: ' + knn_best_vote)
+    # Result for all runs
+    xval_knn_bestdiff = [d[knn_best_diff] for d in result_rf_cross_val]
+    
+#%% Summary of kNN results
+plot_dataset_knn = True
+if plot_dataset_knn:
+    # Get list of datasets
+    dataset_keys = result_knn_cross_val[0].keys() # TODO: consider storing dataset keys in result_summary or other variable
+    n_sets = length(dataset_keys)
+    knn_result_vec = np.zeros((n_sets ,1))
+    for i_run, result_dict in enumerate(result_knn_cross_val):
+        # Get best result
+        best_res = result_summary[i_run]['cross-val_knn_max']
+        for i_dataset, key in enumerate(result_dict):
+            # Find diff from best result
+            knn_result_vec[i_dataset, 0] += result_dict[key] - best_res
+                      
+    fig = plt.figure()
+    plt.title('KNN dataset result, diff from max accuracy')
+    plt.scatter(np.arange(n_sets), knn_result_vec, c='b', marker='o')
+    plt.ylabel('Dataset nr.')
+    plt.show()
+    
+if plot_dataset_rf and plot_dataset_knn:
+    sum_result_vec = rf_result_vec+knn_result_vec
+    
+    fig = plt.figure()
+    plt.title('RF+KNN dataset result, diff from max accuracy')
+    plt.scatter(np.arange(n_sets), sum_result_vec, c='k', marker='o')
+    plt.ylabel('Dataset nr.')
+    plt.show()
+    
+    max_diff_avg = np.max(sum_result_vec)/n_sets
+    print(list(dataset_keys)[np.argmax(sum_result_vec)])
+
+
 #%% PLOT GAIN
 fig = plt.figure()
 plt.scatter(largest_class_size, best_knn_xval_gain, c='r', marker='o')
@@ -90,6 +161,8 @@ fig = plt.figure()
 plt.scatter(largest_class_size, xval_knn_max, c='r')
 plt.plot([0,1], [0,1], c='g')
 plt.xlabel('Largest class size'); plt.ylabel('Accuracy')
+if plot_dataset_knn:
+    plt.scatter(largest_class_size, xval_knn_bestdiff, c='b')
 #plt.gca().set_aspect('equal', adjustable='box')
 plt.ylim((0,1)); plt.xlim((0,1))
 #plt.legend(['other', 'Live', 'Defoliated'])
@@ -149,60 +222,3 @@ if do_cross_set:
     plt.ylim((0,1)); plt.xlim((0,1))
     plt.title('SVM max accuracy - Train on one set, test on others')
     plt.show()
-
-#%% Summary of Random Forest (rf) results
-plot_dataset_rf = True
-if plot_dataset_rf:
-    # Get list of datasets
-    dataset_keys = result_rf_cross_val[0].keys() # TODO: consider storing dataset keys in result_summary or other variable
-    n_sets = length(dataset_keys)
-    rf_result_vec = np.zeros((n_sets ,1))
-    for i_run, result_dict in enumerate(result_rf_cross_val):
-        #
-        #xval_rf_max = [d['cross-val_rf_max'] for d in result_summary]
-        print(i_run)
-        print(result_summary[i_run]['cross-val_rf_max'])
-        best_res = result_summary[i_run]['cross-val_rf_max']
-        for i_dataset, key in enumerate(result_dict):
-            #print(d)
-            rf_result_vec[i_dataset, 0] += result_dict[key] - best_res
-                      
-    fig = plt.figure()
-    plt.title('RF dataset result, diff from max accuracy')
-    plt.scatter(np.arange(n_sets), rf_result_vec, c='r', marker='o')
-    plt.ylabel('Dataset nr.')
-    plt.show()
-    
-    # Find best set
-    
-#%% Summary of kNN results
-plot_dataset_knn = True
-if plot_dataset_knn:
-    # Get list of datasets
-    dataset_keys = result_knn_cross_val[0].keys() # TODO: consider storing dataset keys in result_summary or other variable
-    n_sets = length(dataset_keys)
-    knn_result_vec = np.zeros((n_sets ,1))
-    for i_run, result_dict in enumerate(result_knn_cross_val):
-        # Get best result
-        best_res = result_summary[i_run]['cross-val_knn_max']
-        for i_dataset, key in enumerate(result_dict):
-            # Find diff from best result
-            knn_result_vec[i_dataset, 0] += result_dict[key] - best_res
-                      
-    fig = plt.figure()
-    plt.title('KNN dataset result, diff from max accuracy')
-    plt.scatter(np.arange(n_sets), knn_result_vec, c='b', marker='o')
-    plt.ylabel('Dataset nr.')
-    plt.show()
-    
-if plot_dataset_rf and plot_dataset_knn:
-    sum_result_vec = rf_result_vec+knn_result_vec
-    
-    fig = plt.figure()
-    plt.title('RF+KNN dataset result, diff from max accuracy')
-    plt.scatter(np.arange(n_sets), sum_result_vec, c='k', marker='o')
-    plt.ylabel('Dataset nr.')
-    plt.show()
-    
-    max_diff_avg = np.max(sum_result_vec)/n_sets
-    print(list(dataset_keys)[np.argmax(sum_result_vec)])
