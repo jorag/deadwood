80292f65e654312e55a0ab20954845d2a1e8760f
jorag <jorgen.agersborg@uit.no>
2020-01-16 12:23:20 +0100

    Improve the visual representation of the combined dataset plot
    TODO: This involved hard-coding some things, improve this later.

--- run_classification.py
+++ run_classification.py
@@ -440,43 +440,65 @@ if separate_dataset_plots:
 #%% Mean RF Accuracy - combined plot for all datasets
 if plot_rf_dataset_comp:
      # Disp
-     sar_data_dict = dict(zip(['PGNLM-20191224-1814', 'IDAN_50_C3', 'boxcar_5x5_C3', 'refined_Lee_5x5_C3' ], 
-                              ['PGNLM', 'IDAN', 'boxcar', 'refined Lee'])) 
+     id_list_use = ['A', 'C']
+     c_vec2 = ['r', 'b', 'g']
+     
+     # Percent factor
+     pct_f = 100
+     
+     #sar_names_dataset = ['IDAN_50_C3', 'boxcar_5x5_C3', 'refined_Lee_5x5_C3', 'PGNLM-20191224-1814', 'NDVI', 'optical']
+     #sar_names_display = ['IDAN', 'boxcar', 'refined Lee', 'PGNLM', 'NDVI', 'optical']
+     
+     sar_names_dataset = ['IDAN_50_C3', 'boxcar_5x5_C3', 'refined_Lee_5x5_C3', 'PGNLM-20191224-1814']
+     sar_names_display = ['IDAN', 'boxcar', 'refined Lee', 'PGNLM']
+     
+     opt_names_dataset = ['NDVI', 'optical']
+     n_opt = length(opt_names_dataset)
+     
+     sar_data_dict = dict(zip(sar_names_dataset, sar_names_display)) 
     
      ofs_use = np.copy(ofs)
      plt.figure()
-     x_bars = np.arange(length(rf_accuracy))
-     datakey_list = list(rf_mean_acc.keys())
-     datakey_list.sort()
-     for i_dataset, dataset_id in enumerate(id_list):
+     x_bars = np.arange(length(sar_names_dataset)+ n_opt)
+     #x_bars = np.arange(length(sar_names_dataset))
+     #datakey_list = list(rf_mean_acc.keys())
+     #datakey_list.sort()
+     for i_dataset, dataset_id in enumerate(id_list_use):
         key_list = []
         rf_accuracy = []
         
-        for dataset_key in datakey_list:
-            if dataset_key[-1] == dataset_id:
-                key_list.append(dataset_key)
-                rf_accuracy.append(rf_mean_acc[dataset_key])
-        
+        for dataset_key in sar_names_dataset:
+            key_list.append(dataset_key+'-'+dataset_id)
+            rf_accuracy.append(pct_f*rf_mean_acc[dataset_key+'-'+dataset_id])
+    
         if key_list:
             ofs_use = ofs_use * -1
             # Plot
-            plt.bar(x_bars*2+ofs_use, rf_accuracy , align='center', color=c_vec[i_dataset], alpha=alf)
+            plt.bar(x_bars[:-n_opt]*2+ofs_use, rf_accuracy , align='center', color=c_vec2[i_dataset], alpha=alf)
+            #plt.bar(x_bars*2+ofs_use, rf_accuracy , align='center', color=c_vec[i_dataset], alpha=alf)
             #plt.hlines(np.max(n_class_samples)/np.sum(n_class_samples), -1, 2*length(rf_accuracy))
-        
      
+     key_list = []
+     rf_accuracy = []
+     for dataset_key in opt_names_dataset:
+            key_list.append(dataset_key+'-A')
+            rf_accuracy.append(pct_f*rf_mean_acc[dataset_key+'-'+dataset_id])
+
+     plt.bar(x_bars[-2:]*2, rf_accuracy , align='center', color='g', alpha=alf)
+        
      # Get display names from dict
-     xtick_list = key_list
+     xtick_list = sar_names_display+opt_names_dataset
      #for i_dataset, dataset_id in enumerate(id_list):
          
     
      plt.xticks(x_bars*2, xtick_list )
-     plt.yticks(np.linspace(0.1,1,num=10))
+     plt.yticks(pct_f*np.linspace(0.1,1,num=10))
      plt.grid(True)
-     plt.title('RF, n_trees: '+str(rf_ntrees)+ ', normalization: '+norm_type+
-              '\n Min:'+', live = '+str(min_p_live)+', defo = '+
-               str(min_p_defo)+', trees = '+str(min_tree_live))
-     plt.ylabel('Mean accuracy, n_splits = '+str(crossval_split_k)); plt.ylim((0,1))
-     plt.legend(id_list)
+     #plt.title('RF, n_trees: '+str(rf_ntrees)+ ', normalization: '+norm_type+
+     #         '\n Min:'+', live = '+str(min_p_live)+', defo = '+
+     #          str(min_p_defo)+', trees = '+str(min_tree_live))
+     plt.ylabel('Mean accuracy %, '+str(crossval_split_k)+'-fold cross validation'); plt.ylim((0,pct_f*1))
+     plt.legend(['RS2 25 July 2017', 'RS2 1 August 2017', 'S2 26 July 2017'])
     
      plt.show()
 
